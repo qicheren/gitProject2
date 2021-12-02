903f1aa78222132e7eabfe13d613bd17e1735dac
qicheren <94793119+qicheren@users.noreply.github.com>
2021-12-02 23:34:13 +0800

    Update UserController.java

--- localGitPro/src/com/nnxy/controller/UserController.java
+++ localGitPro/src/com/nnxy/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.nnxy.controller;
 
 public class UserController {
-
+    private int id ; //ID
+    private String username;  //用户名
+    private String passworf;  //密码
+    private int sex;  //性别
+    private String love;  //爱好  
 }
